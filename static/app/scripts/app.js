5d099be4f333dc6407cf6e45ae45d47cb1ce1f5a
jarl-alejandro <jarlalejor@gmail.com>
2015-05-28 12:25:38 -0500

    angular y browserify corren

--- static/app/scripts/app.js
+++ static/app/scripts/app.js
@@ -1,11 +1,22 @@
 (function(){
 
   var angular = require("angular")
-  var app = angular.module("EveryPublished", [])
+  var ngResource = require("angular-resource")
+  var ngRoute = require("angular-route")
 
+  var app = angular.module("EveryPublished", [ngResource, ngRoute])
 
   app.controller("home", ["$scope",function($scope){
     $scope.lol = "Alejandro Rivas";
   }])
+
+  app.config(["$routeProvider",function($routeProvider){
+      $routeProvider
+        .when("/", {
+            controller: "home",
+            templateUrl: "templates/home.html"
+        })
+  }])
+
 
 })()
