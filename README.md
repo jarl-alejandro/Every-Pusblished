cd4281cbdbe5b19da2ec499e5131d12d765da8f6
jarl-alejandro <jarlalejor@gmail.com>
2015-05-29 19:57:17 -0500

    conection DB with mongoDB

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Every-Pusblished
+
+## User
+    {
+        “name”: “Chefly”,
+        “email”: “chefly@gmail.com”,
+        “password”: “157454sad57asd457asd”,
+        “avatar”: “chefly.jpg”,
+        “direction”: “Santo Domingo, Ecuador”,
+        “about”: “Somos una micro-empresa que ofrecemos comida para eventos”,
+    }
--- server.js
+++ server.js
@@ -2,10 +2,20 @@
 
 var port = process.env.PORT || 3000
 var http = require('http')
+var mongoose = require("mongoose")
 var App = require('./lib')
 
 var everyPublised = new App({})
 var server = http.createServer(everyPublised.app)
+
+mongoose.connect("mongodb://localhost/everyPublised", onListenDB)
+
+function onListenDB(err){
+    if(err)
+        return err.message
+    else
+        console.log("DB connect with exited!!! :)")
+}
 
 server.listen(port, function(){
     console.log('App running in http://localhost:'+port)
